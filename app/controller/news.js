17453b9f6aad8ff0850cbace65e28f97632cbe4b
jiaorengui <tiny.jiao@aliyun.com>
2019-10-24 18:34:45 +0800

    add egg 架构

--- app/controller/news.js
+++ app/controller/news.js
@@ -2,7 +2,7 @@
  * @Author: Tiny
  * @Date: 2019-10-23 16:08:40
  * @Last Modified by: tiny.jiao@aliyun.com
- * @Last Modified time: 2019-10-23 16:47:28
+ * @Last Modified time: 2019-10-24 09:59:38
  */
 'use strict';
 
@@ -11,16 +11,16 @@ const Controller = require('egg').Controller;
 class NewsController extends Controller {
   async list() {
     const ctx = this.ctx;
-    const page = ctx.query.page || 1;
-    const newsList = await ctx.service.news.list(page);
-    // const dataList = {
-    //   list: [
-    //     { id: 1, title: 'this is news 1', url: '/news/1' },
-    //     { id: 2, title: 'this is news 2', url: '/news/2' },
-    //   ],
-    // };
-    console.log('news:', await ctx.service.news);
-    await ctx.render('news/list.tpl', { list: newsList });
+    // const page = ctx.query.page || 1;
+    // const newsList = await ctx.service.news.list(page);
+    const dataList = {
+      list: [
+        { id: 1, title: 'this is news 1', url: '/news/1' },
+        { id: 2, title: 'this is news 2', url: '/news/2' },
+      ],
+    };
+    // await ctx.render('news/list.tpl', { list: newsList });
+    await ctx.render('news/list.tpl', dataList);
   }
 }
 
